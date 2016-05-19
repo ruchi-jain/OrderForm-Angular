17c4a3fb91492e4c2222281ad175b2be998b127a
Ruchi Jain <ruchi.goel@outlook.com>
2016-05-18 22:49:07 -0700

    formatted code

--- orderForm.js
+++ orderForm.js
@@ -1,48 +1,46 @@
-function OrderFormController($scope){
+function OrderFormController($scope) {
 
-	// Define the model properties. The view will loop
-	// through the services array and genreate a li
-	// element for every one of its items.
+    // Define the model properties. The view will loop
+    // through the services array and genreate a li
+    // element for every one of its items.
 
-	$scope.services = [
-		{
-			name: 'Web Development',
-			price: 300,
-			active:true
-		},{
-			name: 'Design',
-			price: 400,
-			active:false
-		},{
-			name: 'Integration',
-			price: 250,
-			active:false
-		},{
-			name: 'Training',
-			price: 220,
-			active:false
-		}
-	];
+    $scope.services = [{
+        name: 'Web Development',
+        price: 300,
+        active: true
+    }, {
+        name: 'Design',
+        price: 400,
+        active: false
+    }, {
+        name: 'Integration',
+        price: 250,
+        active: false
+    }, {
+        name: 'Training',
+        price: 220,
+        active: false
+    }];
 
-	$scope.toggleActive = function(s){
-		s.active = !s.active;
-	};
+    $scope.toggleActive = function(s) {
+        s.active = !s.active;
+    };
 
-	// Helper method for calculating the total price
+    // Helper method for calculating the total price
 
-	$scope.total = function(){
+    $scope.total = function() {
 
-		var total = 0;
+        var total = 0;
 
-		// Use the angular forEach helper method to
-		// loop through the services array:
+        // Use the angular forEach helper method to
+        // loop through the services array:
 
-		angular.forEach($scope.services, function(s){
-			if (s.active){
-				total+= s.price;
-			}
-		});
+        angular.forEach($scope.services, function(s) {
+            if (s.active) {
+                total += s.price;
+            }
+        });
 
-		return total;
-	};
+        return total;
+    };
 }
